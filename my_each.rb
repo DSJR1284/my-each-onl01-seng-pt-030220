316fb73c0d73c4a0ad686f77681744c9f0a2309c
Doug Jones <dsjr1284@gmail.com>
2020-03-13 16:11:27 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(arg)
+   if block_given? 
+   i = 0
+  
+  while i < arg.length
+    i + = 1 
+    yield (arg[i])
+  end
+    arg
 end
+
+
+  
